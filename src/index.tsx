45942c39940f2d1c7452d8880d26e89de9ad75e7
JiHyoung You <fbwlgud51@gmail.com>
2025-06-03 00:01:02 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  // 번들은 srs밑에 있는 index 파일을 사용하기 떄문에 content로 수정.
+  // document.getElementById('root') as HTMLElement
+  document.getElementById("content") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
