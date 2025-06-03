f677cc3012ac56ddfd597a9334d6619bcec89982
JiHyoung You <fbwlgud51@gmail.com>
2025-06-04 03:28:06 +0900

    fix: 컴포넌트 폴더로 분리

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,14 @@
-import { Box, styled } from "@mui/material";
-import React from "react";
+import { styled } from "@mui/material";
 import { NavLink, Outlet } from "react-router";
 import spotifyLogo from "../assets/logo/Spotify_Logo_White.png";
 import "./styles/AppLayout.style.css";
-import { transform } from "typescript";
+import LibraryHead from "./components/LibraryHead";
+import EmptyPlaylist from "./components/EmptyPlaylist";
+import NotPlayingBar from "./components/NotPlayingBar";
 
 const LayoutContainer = styled("div")({
   display: "flex",
+  height: "100vh",
   flexDirection: "column",
 });
 
@@ -66,17 +68,17 @@ const SideBar = styled("div")(({ theme }) => ({
 
 const MainContainer = styled("div")({
   display: "flex",
-  height: "100vh",
+  height: "100%",
   paddingInline: "8px",
+  marginBottom: "82px",
   gap: "8px",
 });
 
-const ContentBox = styled(Box)(({ theme }) => ({
+const ContentBox = styled("div")(({ theme }) => ({
   borderRadius: "8px",
   backgroundColor: theme.palette.background.paper,
   color: theme.palette.text.primary,
   width: "100%",
-  padding: "8px",
   marginBottom: "8px",
   marginRight: "8px",
 }));
@@ -85,7 +87,13 @@ const AppLayout = () => {
   return (
     <LayoutContainer>
       <NavBar>
-        <img src={spotifyLogo} alt="spotify logo" className="layout-logo-img" />
+        <NavIcon to="/">
+          <img
+            src={spotifyLogo}
+            alt="spotify logo"
+            className="layout-logo-img"
+          />
+        </NavIcon>
         <div className="container-center">
           <NavIconArea>
             <NavIcon to="/">
@@ -114,7 +122,6 @@ const AppLayout = () => {
                   <path d="M10.533 1.27893C5.35215 1.27893 1.12598 5.41887 1.12598 10.5579C1.12598 15.697 5.35215 19.8369 10.533 19.8369C12.767 19.8369 14.8235 19.0671 16.4402 17.7794L20.7929 22.132C21.1834 22.5226 21.8166 22.5226 22.2071 22.132C22.5976 21.7415 22.5976 21.1083 22.2071 20.7178L17.8634 16.3741C19.1616 14.7849 19.94 12.7634 19.94 10.5579C19.94 5.41887 15.7138 1.27893 10.533 1.27893ZM3.12598 10.5579C3.12598 6.55226 6.42768 3.27893 10.533 3.27893C14.6383 3.27893 17.94 6.55226 17.94 10.5579C17.94 14.5636 14.6383 17.8369 10.533 17.8369C6.42768 17.8369 3.12598 14.5636 3.12598 10.5579Z"></path>
                 </svg>
               </NavIcon>
-
               <input
                 className="layout-search-input"
                 placeholder="어떤 콘텐츠를 감상하고 싶으세요?"
@@ -159,10 +166,14 @@ const AppLayout = () => {
       </NavBar>
       <MainContainer>
         <SideBar>
-          <ContentBox>SideBar</ContentBox>
+          <ContentBox>
+            <LibraryHead />
+            <EmptyPlaylist />
+          </ContentBox>
         </SideBar>
         <Outlet />
       </MainContainer>
+      <NotPlayingBar />
     </LayoutContainer>
   );
 };
