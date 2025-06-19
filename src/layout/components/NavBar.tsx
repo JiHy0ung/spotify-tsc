e3d0740d9a9a3c0206b88c73944d418eac9ce8c0
JiHyoung You <fbwlgud51@gmail.com>
2025-06-20 02:49:20 +0900

    style: (Nav바) 앱 설치 아이콘 페이지 이동 추가

--- src/layout/components/NavBar.tsx
+++ src/layout/components/NavBar.tsx
@@ -259,13 +259,35 @@ const NavBarInfoSecond = styled("div")({
   "& a": {
     padding: "4px 16px 4px 8px",
     color: "#b3b3b3",
-
     cursor: "pointer",
     textDecoration: "none",
     "&:visited": {
       color: "#b3b3b3",
     },
     "&:hover": {
+      color: "#ffffff",
+    },
+  },
+});
+
+const NavBarLinkArea = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  "& a": {
+    padding: "4px 16px 4px 8px",
+    color: "#b3b3b3",
+    cursor: "pointer",
+    textDecoration: "none",
+    "&:visited": {
+      color: "#b3b3b3",
+    },
+  },
+  "&:hover": {
+    "& svg": {
+      fill: "#ffffff",
+    },
+    "& a": {
       color: "#ffffff",
     },
   },
@@ -458,15 +480,23 @@ const NavBar = () => {
               <NavContainerCenter sx={{ paddingInline: "8px" }}>
                 <NavInfoSecond>
                   <NavContainerCenter sx={{ gap: "8px" }}>
-                    <svg
-                      aria-hidden="true"
-                      viewBox="0 0 16 16"
-                      className="layout-nav-download-img"
-                    >
-                      <path d="M4.995 8.745a.75.75 0 0 1 1.06 0L7.25 9.939V4a.75.75 0 0 1 1.5 0v5.94l1.195-1.195a.75.75 0 1 1 1.06 1.06L8 12.811l-.528-.528a.945.945 0 0 1-.005-.005L4.995 9.805a.75.75 0 0 1 0-1.06z"></path>
-                      <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13z"></path>
-                    </svg>
-                    앱 설치하기
+                    <NavBarLinkArea>
+                      <svg
+                        aria-hidden="true"
+                        viewBox="0 0 16 16"
+                        className="layout-nav-download-img"
+                      >
+                        <path d="M4.995 8.745a.75.75 0 0 1 1.06 0L7.25 9.939V4a.75.75 0 0 1 1.5 0v5.94l1.195-1.195a.75.75 0 1 1 1.06 1.06L8 12.811l-.528-.528a.945.945 0 0 1-.005-.005L4.995 9.805a.75.75 0 0 1 0-1.06z"></path>
+                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13z"></path>
+                      </svg>
+                      <a
+                        href="https://www.spotify.com/download"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        앱 설치하기
+                      </a>
+                    </NavBarLinkArea>
                   </NavContainerCenter>
 
                   <NavContainerCenter>
